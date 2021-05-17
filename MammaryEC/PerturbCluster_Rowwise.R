9f48b69f7ffa4f255083b3eab867657f05c05c15
saschajung <sascha.zickenrott@googlemail.com>
2021-05-17 09:20:22 +0200

    Added Missing files and fixed bugs
    Fixed a commented line in PerturbCluster_Rowwise.R and removed unnecessary lines at the end.

--- MammaryEC/PerturbCluster_Rowwise.R
+++ MammaryEC/PerturbCluster_Rowwise.R
@@ -7,16 +7,15 @@ pertSize = 4
 setwd("./MammaryEC/")
 network <- read.table(paste0("./MammaryEC_Network_v1.txt"),header = TRUE, sep=',',stringsAsFactors = FALSE)
 #Read prior (p-values, correspond to probability of being zero)
-prior <- read.table(paste0("../../NK_Newest/iPSC_GSM1088317_TPM_bool_ecdf.csv"),header=TRUE,stringsAsFactors = FALSE)
+prior <- read.table(paste0("./iPSC_GSM1088317_TPM_bool_ecdf.csv"),header=TRUE,stringsAsFactors = FALSE)
 prior <- prior[prior$HGNC_symbol %in% network$Gene,]
 prior <- prior[order(match(prior$HGNC_symbol,network$Gene)),]
 tmp1 <- tempfile(tmpdir = "./")
 tmp2 <- tempfile(tmpdir = "./")
 tmp3 <- tempfile(tmpdir = "./")
-setwd("~/Downloads/core_networks/output_files/")
 system(paste0("cut -f6,7,8,9 ./MammaryEC_Shell1_Pro_Acc_OnlyTF.bed | sort | uniq | intersectBed -u -b ./iPSC_K4me3_*.bed -a - > ",tmp1))
-system(paste0("intersectBed -u -a ../Processed_GeneHancer_GRCh38.bed -b ./MammaryEC_K27ac_*.bed > ",tmp2))
-system(paste0("intersectBed -u -a ../Processed_GeneHancer_GRCh38.bed -b ./iPSC_K27ac_*.bed > ",tmp3))
+system(paste0("intersectBed -u -a ./Processed_GeneHancer_GRCh38.bed -b ./MammaryEC_K27ac_*.bed > ",tmp2))
+system(paste0("intersectBed -u -a ./Processed_GeneHancer_GRCh38.bed -b ./iPSC_K27ac_*.bed > ",tmp3))
 print("part 1 done")
 Final_Prom_Active_In_Start <- read.table(tmp1, header = FALSE, stringsAsFactors = FALSE)
 Final_Enhancers <- read.table(tmp2, header = FALSE, stringsAsFactors = FALSE)
@@ -51,20 +50,15 @@ while(TRUE){
   #Check which perturbation fullfills the criteria
   pert_idx <- which(sapply(seq(1,ncol(pp)),function(x){all(line_vec[which(perturbagens$V1 %in% pp[,x])] == 1)}))
   for(idx in pert_idx){
-    zeros <- which(line_vec[1:(length(line_vec)-1)] == 0)
-    ones <- setdiff(which(line_vec[1:(length(line_vec)-1)] == 1),which(perturbagens$V1 %in% pp[,idx]))
+    zeros <- which(line_vec == 0)
+    ones <- setdiff(which(line_vec == 1),which(perturbagens$V1 %in% pp[,idx]))
     stateProb <- prod(1-prior$ecdf[zeros])*prod(prior$ecdf[ones])
-    chromChangeProb <- 1#prod((commonEnhancers[zeros]/totalEnhancers[zeros]))*prod(1-(commonEnhancers[ones]/totalEnhancers[ones]))
-    prob <- tail(line_vec,1)#ifelse(is.na(tail(line_vec,1)),0,1/tail(line_vec,1))
+    chromChangeProb <- prod(1-commonEnhancers[zeros]/totalEnhancers[zeros])*prod(commonEnhancers[ones]/totalEnhancers[ones])
+    prob <- ifelse(is.na(tail(line_vec,1)),0,1/tail(line_vec,1))
     nominator[idx] <- nominator[idx] + stateProb*chromChangeProb*prob
     denominator[idx] <- denominator[idx] + stateProb*chromChangeProb
   }
 }
 close(con)
 nd <- nominator/denominator
-wTP53 <- c(wTP53,nd[apply(pp,2,function(x){any(levels(pp)[x] == "TP53")})])
-woTP53 <- c(woTP53,nd[!apply(pp,2,function(x){any(levels(pp)[x] == "TP53")})])
-
-wTP53 <- c()
-woTP53 <- c()
-save(nominator,denominator,pp,file = paste0("./FinalVals_6_",args[1],".RData"))
+save(nominator,denominator,pp,nd,file = paste0("./FinalVals_",pertSize,"_",args[1],".RData"))
